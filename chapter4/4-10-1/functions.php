9b08866fab934949cc41defabb0f2ff453a9714f
ttctst <87161699+ttctst@users.noreply.github.com>
2022-02-25 03:38:55 +0800

    Create functions.php

---
+++ chapter4/4-10-1/functions.php
@@ -0,0 +1,13 @@
+<?php
+$query = new WP_Query( 'year=2012&monthnum=12&day=12' );
+ $args = array(
+ 'date_query' => array(
+   array(
+   'year' => 2012,
+   'month' => 12,
+   'day' => 12,
+    ),
+  ),
+);
+
+$query = new WP_Query( $args );
